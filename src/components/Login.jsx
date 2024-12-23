402369a6e7eedb24a0a77235f3cc391209458d46
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-23 14:38:58 +0530

    Sign In / Sign Up Form Toggle

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,10 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 function Login() {
+  const [IsSignin, setIsSignin] = React.useState(true);
+  function toggleForm() {
+    setIsSignin(!IsSignin);
+  }
   return (
     <>
       <div
@@ -16,12 +20,28 @@ function Login() {
       >
         <div className="w-full h-screen flex flex-wrap justify-center bg-white/10 backdrop-blur-lg sm:bg-transparent sm:backdrop-blur-none">
           <Header />
-          <div className="w-full sm:w-2/3 lg:w-1/4 sm:h-2/3 sm:bg-white/10 sm:backdrop-blur-lg rounded-lg shadow-lg p-8">
+          <div
+            className="w-full sm:w-2/3 lg:w-1/4 sm:h-auto sm:bg-white/10 sm:backdrop-blur-lg rounded-lg sm:shadow-lg p-8 mb-20
+        "
+          >
             {" "}
             <h1 className="font-title text-white text-3xl mb-6 font-semibold">
-              Sign In
+              {IsSignin ? "Sign In" : "Sign Up"}
             </h1>
             <form className="flex flex-col gap-4">
+              {!IsSignin && (
+                <div className="flex flex-col">
+                  <label htmlFor="email" className="text-white text-sm mb-2">
+                    Name
+                  </label>
+                  <input
+                    type="Name"
+                    id="Name"
+                    className="p-3 rounded-md bg-white/50 focus:outline-none focus:ring-2 focus:ring-white/80"
+                    placeholder="Enter your Name"
+                  />
+                </div>
+              )}
               <div className="flex flex-col">
                 <label htmlFor="email" className="text-white text-sm mb-2">
                   Email
@@ -48,14 +68,17 @@ function Login() {
                 type="submit"
                 className="bg-[#e50914] text-white p-2 rounded-md shadow-xl w-full active:bg-red-400"
               >
-                Sign In
+                {IsSignin ? "Sign In" : "Sign Up"}
               </button>
             </form>
             <p className="text-white text-sm mt-6 text-left">
-              New to MovieGPT?{" "}
-              <a href="#" className="text-primary-500 underline">
-                Sign up
-              </a>
+              {IsSignin ? "New to MovieGPT? " : "Already have an account? "}
+              <span
+                className="text-primary-500 underline cursor-pointer"
+                onClick={toggleForm}
+              >
+                {IsSignin ? "Sign Up" : "Sign In"}
+              </span>
             </p>
           </div>
         </div>
