b4f882555729fd882483d40b7e2a58c3f319c0f1
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-30 01:48:43 +0530

    reCAPTCHA Implemented

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,66 +1,84 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Header } from "../components";
 import { Footer } from "../components";
 import { authService, checkValidateData } from "../utils";
-import { useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
-//To get data from input boxes we can either use  onChange or onInput with state variables or use useRef to use reference of those input boxe
-//Add name validation
+const reCaptchaKey = import.meta.env.VITE_SITE_KEY;
+
 function Login() {
-  const [IsSignin, setIsSignin] = React.useState(true);
-  const [error, setError] = React.useState(null);
+  const [isSignin, setIsSignin] = useState(true);
+  const [error, setError] = useState(null);
+  const [recaptchaVerified, setRecaptchaVerified] = useState(false);
 
-  function toggleForm() {
-    setIsSignin(!IsSignin);
-  }
   const email = useRef(null);
   const password = useRef(null);
   const name = useRef(null);
 
+  const handleRecaptchaChange = (value) => {
+    if (value) {
+      setRecaptchaVerified(true);
+      setError(null); // Clear error if reCAPTCHA is verified
+    } else {
+      setRecaptchaVerified(false);
+    }
+  };
+
+  const toggleForm = () => setIsSignin(!isSignin);
+
   async function handleSignIn() {
-    const error = await authService.signIn(
-      email.current.value,
-      password.current.value
-    );
-    setError(`${error.errorCode} - ${error.errorMessage}`);
+    if (!recaptchaVerified) {
+      setError("Please complete the reCAPTCHA verification.");
+      return;
+    }
+    try {
+      const error = await authService.signIn(
+        email.current.value,
+        password.current.value
+      );
+      setError(`${error.errorCode} - ${error.errorMessage}`);
+    } catch (err) {
+      setError("Failed to sign in. Please try again.");
+    }
   }
 
   async function handleSignUp() {
-    const error = await authService.signUp(
-      email.current.value,
-      password.current.value,
-      name.current.value
-    );
-
-    setError(`${error.errorCode} - ${error.errorMessage}`);
+    if (!recaptchaVerified) {
+      setError("Please complete the reCAPTCHA verification.");
+      return;
+    }
+    try {
+      const error = await authService.signUp(
+        email.current.value,
+        password.current.value,
+        name.current.value
+      );
+      setError(`${error.errorCode} - ${error.errorMessage}`);
+    } catch (err) {
+      setError("Failed to sign up. Please try again.");
+    }
   }
 
   function handleButtonClick() {
-    //console.log(email)
-    // console.log(email.current.value);
-    // console.log(password.current.value);
-    //console.log(password)
-    const message = checkValidateData(
+    const validationError = checkValidateData(
       email.current.value,
       password.current.value
     );
-    setError(message);
-    //Auth Logic
-    if (message) {
+    if (validationError) {
+      setError(validationError);
       return;
     }
-    if (IsSignin) {
-      //Sign In Logic
+    if (isSignin) {
       handleSignIn();
     } else {
-      //Sign Up Logic
       handleSignUp();
     }
   }
+
   return (
     <>
       <div
-        className="w-full h-screen flex flex-wrap justify-center bg-cover bg-no-repeat"
+        className="w-full h-[110vh] flex flex-wrap justify-center bg-cover bg-no-repeat"
         style={{
           backgroundImage: `url(background.jpeg)`,
           backgroundSize: "100% 100%",
@@ -68,31 +86,27 @@ function Login() {
           backgroundPosition: "center",
         }}
       >
-        <div className="w-full h-screen flex flex-wrap justify-center bg-white/10 backdrop-blur-lg sm:bg-transparent sm:backdrop-blur-none">
+        <div className="w-full h-[110vh] flex flex-wrap justify-center bg-white/10 backdrop-blur-lg sm:bg-transparent sm:backdrop-blur-none">
           <Header />
-          <div
-            className="w-full sm:w-2/3 lg:w-1/4 sm:h-auto sm:bg-white/10 sm:backdrop-blur-lg rounded-lg sm:shadow-lg p-8 mb-20
-        "
-          >
-            {" "}
+          <div className="w-full sm:w-2/3 lg:w-1/4 sm:h-auto sm:bg-white/10 sm:backdrop-blur-lg rounded-lg sm:shadow-lg p-8 mb-20">
             <h1 className="font-title text-white text-3xl mb-6 font-semibold">
-              {IsSignin ? "Sign In" : "Sign Up"}
+              {isSignin ? "Sign In" : "Sign Up"}
             </h1>
             <form
               onSubmit={(e) => e.preventDefault()}
               className="flex flex-col gap-4"
             >
-              {!IsSignin && (
+              {!isSignin && (
                 <div className="flex flex-col">
-                  <label htmlFor="email" className="text-white text-sm mb-2">
+                  <label htmlFor="name" className="text-white text-sm mb-2">
                     Name
                   </label>
                   <input
                     ref={name}
-                    type="Name"
-                    id="Name"
+                    type="text"
+                    id="name"
                     className="p-3 rounded-md bg-white/50 focus:outline-none focus:ring-2 focus:ring-white/80"
-                    placeholder="Enter your Name"
+                    placeholder="Enter your name"
                   />
                 </div>
               )}
@@ -120,22 +134,31 @@ function Login() {
                   placeholder="Enter your password"
                 />
               </div>
+              <ReCAPTCHA
+                sitekey={reCaptchaKey}
+                onChange={handleRecaptchaChange}
+                style={{
+                  transform: "scaleY(0.7) scaleX(0.85)",
+                  alignSelf: "center",
+                }}
+              />
               <p className="text-[#e50914]">{error}</p>
               <button
                 type="submit"
                 onClick={handleButtonClick}
                 className="bg-[#e50914] text-white p-2 rounded-md shadow-xl w-full active:bg-red-400"
+                disabled={!recaptchaVerified}
               >
-                {IsSignin ? "Sign In" : "Sign Up"}
+                {isSignin ? "Sign In" : "Sign Up"}
               </button>
             </form>
             <p className="text-white text-sm mt-6 text-left">
-              {IsSignin ? "New to MovieGPT? " : "Already have an account? "}
+              {isSignin ? "New to MovieGPT? " : "Already have an account? "}
               <span
                 className="text-primary-500 underline cursor-pointer"
                 onClick={toggleForm}
               >
-                {IsSignin ? "Sign Up" : "Sign In"}
+                {isSignin ? "Sign Up" : "Sign In"}
               </span>
             </p>
           </div>
